de2143067977c2c91c6d9bb5b2b855ec2071ae48
eltoro007 <anandita.rohi@gmail.com>
2017-07-17 16:02:06 +0530

    Update MainController.java

--- MediaPlayer/src/application/MainController.java
+++ MediaPlayer/src/application/MainController.java
@@ -33,25 +33,33 @@ public class MainController implements Initializable {
 	private MediaView mv;
 	private Media me;
 	@FXML
-	private ListView list;
+	private ListView<String> list;
 	
 	
 	private MediaPlayer mp;
+	private String curfilepath;// Store the currently playing filename separately
 	private String filepath;
+	
 	
 	@FXML
 	private void handleButtonAction(ActionEvent event)
 	{
 		FileChooser filechooser=new FileChooser();
-		FileChooser.ExtensionFilter filter=new FileChooser.ExtensionFilter("Select a file", "*.mp3","*.m4a","*.wav","*.mkv");
+		FileChooser.ExtensionFilter filter=new FileChooser.ExtensionFilter("Select a file", "*.mp3","*.m4a","*.wav");
 		filechooser.getExtensionFilters().add(filter);
 		File file=filechooser.showOpenDialog(null);
-		filepath=file.toURI().toString();
+		filepath = file.toURI().toString();
 		if(filepath!=null){
 			list.getItems().add(file.getName());
-		Media me=new Media(filepath);
-		mp=new MediaPlayer(me);
-		mv.setMediaPlayer(mp);
+			
+		if(!(curfilepath!=null))
+		{
+			// Play the new file only if it is the first file being loaded
+			me=new Media(filepath);
+			mp=new MediaPlayer(me);
+			mv.setMediaPlayer(mp);
+			curfilepath = filepath;
+		}
 		
 		slider.setValue(mp.getVolume()*100);
 		slider.valueProperty().addListener(new InvalidationListener(){
@@ -72,6 +80,11 @@ public class MainController implements Initializable {
 			
 		});
 		
+					
+										
+		
+		
+		
 		seekslider.setOnMouseClicked(new EventHandler<MouseEvent>(){
 			
 			@Override
@@ -85,6 +98,55 @@ public class MainController implements Initializable {
 		
 		
 		mp.play();
+		list.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			
+		    @Override
+		    public void handle(MouseEvent click) {
+		        if (click.getClickCount() == 2) {
+		        	// Double click on a list item.
+		      
+		            String currentItemSelected = list.getSelectionModel().getSelectedItem();
+		          
+		               mp.stop();
+		               currentItemSelected=file.toURI().toString();
+		               
+		       			me=new Media(currentItemSelected);
+		       			mp=new MediaPlayer(me);
+		       			mv.setMediaPlayer(mp);
+		       		
+		       			mp.play();
+		       			mp.currentTimeProperty().addListener(new ChangeListener<Duration>(){
+		       				@Override
+		       				public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue){
+		       					seekslider.setValue(newValue.toSeconds());
+		       				}
+		       				
+		       				
+		       			});
+		       			
+		       						
+		       											
+		       			
+		       			
+		       			
+		       			seekslider.setOnMouseClicked(new EventHandler<MouseEvent>(){
+		       				
+		       				@Override
+		       				public void handle(MouseEvent event){
+		       					mp.seek(Duration.seconds(seekslider.getValue()));
+		       					
+		       				}
+		       				
+		       			});
+		            	   
+		       			
+		    			
+		    		
+		            
+		        }
+		    }
+		});
+
 		
 		}
 
@@ -121,10 +183,12 @@ public class MainController implements Initializable {
 	}
 	public void fast(ActionEvent event)
 	{
+		// Probably better to replace this with the Next track functionality
 		mp.setRate(2);
 	}
 	public void slow(ActionEvent event)
 	{
+		// Probably better to replace this with Previous track functionality
 		mp.setRate(0.5);
 	}
 	public void reload(ActionEvent event)
